532c704a596a873c35206e15659f5f95b3b72005
Leonardo Magno Marones Lopes <leonardo.marones@mobicare.com.br>
2021-06-04 14:42:04 -0300

    - Formatação de dados.

--- app/src/main/java/alura/com/br/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/alura/com/br/ui/activity/ListaPacotesActivity.java
@@ -18,6 +18,7 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        setTitle("Pacotes");
 
         ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
 
